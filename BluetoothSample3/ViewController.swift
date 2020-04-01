9090bd33fcc3efa7e56355c24ff64a79dc31b910
Takuya Yokoyama <chocoyama@yahoo-corp.jp>
2020-04-01 12:06:51 +0900

    Initial Commit

---
+++ BluetoothSample3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothSample3
+//
+//  Created by Takuya Yokoyama on 2020/04/01.
+//  Copyright © 2020 Takuya Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
